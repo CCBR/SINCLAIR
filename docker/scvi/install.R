c9ba92205eb8d688f6848e29240a7f26de3b0afe
Kelly Sovacool <kelly.sovacool@nih.gov>
2024-10-24 17:15:48 -0400

    ci: pass github PAT for remotes rate limit

--- docker/scvi/install.R
+++ docker/scvi/install.R
@@ -1,3 +1,7 @@
+#!/usr/bin/env Rscript
+args <- commandArgs(trailingOnly = TRUE)
+sys.setenv(GITHUB_PAT = args[1])
+
 setRepositories(ind = 1:3)
 remotes::install_github("satijalab/seurat-wrappers")
 remotes::intall_cran("BPCells", repos = c("https://bnprks.r-universe.dev"))
