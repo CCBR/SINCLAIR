f05274f3d1a1ad08bfdb018330babbb46858e176
Kelly Sovacool <kelly.sovacool@nih.gov>
2024-10-24 17:45:21 -0400

    fix: typos

--- docker/integ/install.R
+++ docker/integ/install.R
@@ -4,4 +4,4 @@ Sys.setenv(GITHUB_PAT = args[1])
 
 setRepositories(ind = 1:3)
 remotes::install_github("satijalab/seurat-wrappers")
-remotes::intall_cran("BPCells", repos = c("https://bnprks.r-universe.dev"))
+remotes::install_cran("BPCells", repos = c("https://bnprks.r-universe.dev"))
